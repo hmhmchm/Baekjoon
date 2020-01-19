05d7b569ad7afb7bd34b2178b09facf447714cae
HyeonMin Choi <58718743+hmhmchm@users.noreply.github.com>
2020-01-19 21:44:36 +0900

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"\\    /\\\n" +
+				" )  ( ')\n" +
+				"(  /  )\n" +
+				" \\(__)|"
+				);
+		
+	}
+
+}
