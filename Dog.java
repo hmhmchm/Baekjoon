779f424ed2b37f1ba13db91f919f5761099c8333
HyeonMin Choi <58718743+hmhmchm@users.noreply.github.com>
2020-01-19 21:45:09 +0900

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"|\\_/|\n" +
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" +
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|"
+				);
+		
+	}
+
+}
