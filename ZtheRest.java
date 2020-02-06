224df422f11ab898baa2220f43213705a9f82aa9
Hyeonmin Choi <hyeonminchoi5@gmail.com>
2020-02-06 21:05:21 +0900

    Update ZtheRest.java

--- ZtheRest.java
+++ ZtheRest.java
@@ -6,6 +6,7 @@ public class TheRest {
 		Scanner in = new Scanner(System.in);
 		int[] rest = new int[10]; //나머지 배열 생성
 		//int[] bowl = new int[10]; 
+		//
 		int count; // 나머지 몇갠지 카운트 
 		for(int i = 0; i < rest.length; i++) {
 			rest[i] = in.nextInt()%42;//여기에 42 나눈 나머지 값 저장하고
